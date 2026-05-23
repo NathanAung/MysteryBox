d821c031f35421723bdc957b234dc0e665567c1b
NathanAung <61079560+NathanAung@users.noreply.github.com>
2026-05-23 17:22:50 +0900

    project creation

---
+++ Source/MysteryBox/MysteryBox.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MysteryBox.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MysteryBox, "MysteryBox" );
